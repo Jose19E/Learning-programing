d9b865db03372c016bba8360c9026db938967f54
Jose E. <jwespinosa1903@gmail.com>
2026-02-24 10:25:30 -0500

    YOUR TECHNICAL MESSAGE

--- python/data_types.py
+++ python/data_types.py
@@ -1,1 +1,14 @@
-print("Hello everyone class")
+#This is my first Python Script
+'''
+Developer: Jose E. 
+Description: This script let us understand how to is data types.
+'''
+
+print("Hello everyone. Welcome to data types class")
+
+age = 17
+print(type(age))
+EARTH_GRAVITY = 9.8
+print(type(EARTH_GRAVITY))
+PI = 3.1416
+print(type(PI))
---
+++ python/variable.py
@@ -0,0 +1,6 @@
+firstname = "Jose"
+
+
+print(type(firstname)) 
+
+print("your name is:" ,firstname)
